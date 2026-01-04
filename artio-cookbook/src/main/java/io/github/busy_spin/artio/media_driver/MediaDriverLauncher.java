012cb9c2b1621df73f12a2c324274ee16c8f06f6
isuru <isurudevj@gmail.com>
2026-01-04 12:34:14 +0800

    initial commit.

--- artio-cookbook/src/main/java/io/github/busy_spin/artio/media_driver/MediaDriverLauncher.java
+++ artio-cookbook/src/main/java/io/github/busy_spin/artio/media_driver/MediaDriverLauncher.java
@@ -3,7 +3,9 @@ package io.github.busy_spin.artio.media_driver;
 import io.aeron.driver.MediaDriver;
 import io.github.busy_spin.artio.AppLauncher;
 import io.github.busy_spin.artio.AppLauncherUtils;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MediaDriverLauncher implements AppLauncher {
 
     MediaDriver mediaDriver;
@@ -15,13 +17,15 @@ public class MediaDriverLauncher implements AppLauncher {
 
     @Override
     public void launch() {
-        mediaDriver = MediaDriver.launch(new MediaDriver.Context());
+        mediaDriver = MediaDriver.launch(new MediaDriver.Context()
+                .printConfigurationOnStart(true));
     }
 
     @Override
     public void close() throws Exception {
         if (mediaDriver != null) {
             mediaDriver.close();
+            log.info("MediaDriver closed successfully");
         }
     }
 }
