3083f0cb5664f5f2e817aae64536217fa11255e9
isuru <isurudevj@gmail.com>
2026-01-04 22:45:28 +0800

    deployment done.

--- artio-cookbook/src/main/java/io/github/busy_spin/artio/fix_engine/FixEngineLauncher.java
+++ artio-cookbook/src/main/java/io/github/busy_spin/artio/fix_engine/FixEngineLauncher.java
@@ -1,18 +1,39 @@
 package io.github.busy_spin.artio.fix_engine;
 
+import io.aeron.Aeron;
 import io.github.busy_spin.artio.AppLauncher;
+import io.github.busy_spin.artio.utils.ThreadFactoryUtils;
+import org.agrona.concurrent.NoOpIdleStrategy;
+import uk.co.real_logic.artio.engine.EngineConfiguration;
+import uk.co.real_logic.artio.engine.FixEngine;
 
 public class FixEngineLauncher implements AppLauncher {
 
-
+    private FixEngine fixEngine;
 
     @Override
     public void launch() {
+        EngineConfiguration configuration = new EngineConfiguration();
+        configuration
+                .bindTo(System.getProperty("artio.host"), Integer.getInteger("artio.port"))
+                .libraryAeronChannel(Aeron.Context.IPC_CHANNEL)
+                .deleteLogFileDirOnStart(true)
+                .logFileDir(System.getProperty("artio.logs.dir"))
+                .logInboundMessages(false)
+                .logOutboundMessages(false)
+                .threadFactory(ThreadFactoryUtils.newDeamonThreadFactory())
+                .framerIdleStrategy(new NoOpIdleStrategy())
+                .monitoringThreadIdleStrategy(new NoOpIdleStrategy())
+                .archiverIdleStrategy(new NoOpIdleStrategy())
+                .printAeronStreamIdentifiers(true);
 
+        fixEngine = FixEngine.launch(configuration);
     }
 
     @Override
     public void close() throws Exception {
-
+        if (fixEngine != null) {
+            fixEngine.close();
+        }
     }
 }
