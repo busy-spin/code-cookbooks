ea1502151a1d87e46b4314072de56b26361741a6
isuru <isurudevj@gmail.com>
2026-01-11 21:43:45 +0800

    deployment done.

--- artio-cookbook/src/main/java/io/github/busy_spin/artio/fix_initiator/FixInitiatorLauncher.java
+++ artio-cookbook/src/main/java/io/github/busy_spin/artio/fix_initiator/FixInitiatorLauncher.java
@@ -3,12 +3,16 @@ package io.github.busy_spin.artio.fix_initiator;
 import io.aeron.Aeron;
 import io.github.busy_spin.artio.AppLauncher;
 import io.github.busy_spin.artio.utils.ThreadFactoryUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.agrona.concurrent.AgentRunner;
+import org.agrona.concurrent.SleepingIdleStrategy;
 import uk.co.real_logic.artio.library.AcquiringSessionExistsHandler;
 import uk.co.real_logic.artio.library.FixLibrary;
 import uk.co.real_logic.artio.library.LibraryConfiguration;
 
 import java.util.Collections;
 
+@Slf4j
 public class FixInitiatorLauncher implements AppLauncher {
 
     private FixLibrary fixLibrary;
@@ -25,6 +29,10 @@ public class FixInitiatorLauncher implements AppLauncher {
                 .inboundLibraryStream(Integer.getInteger("artio.inbound.library.stream"))
                 .outboundLibraryStream(Integer.getInteger("artio.outbound.library.stream"));
         fixLibrary = FixLibrary.connect(configuration);
+
+        new AgentRunner(new SleepingIdleStrategy(), throwable -> {
+            log.error("Error launching fix initiator", throwable);
+        }, null, new InitiatorAgent(new FixTestRequestHandler()));
     }
 
     @Override
